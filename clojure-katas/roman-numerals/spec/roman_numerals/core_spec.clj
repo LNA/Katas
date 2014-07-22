6323da756d3f6aaf1a0bfe3995662e4aaf2abc3d
LNA <LAllen@8thLight.com>
2014-07-22 15:52:35 -0500

    Adds the third and fourth tests

--- clojure-katas/roman-numerals/spec/roman_numerals/core_spec.clj
+++ clojure-katas/roman-numerals/spec/roman_numerals/core_spec.clj
@@ -2,8 +2,12 @@
   (:require [speclj.core :refer :all]
             [roman-numerals.core :refer :all]))
 (describe "roman-numerals"
-          (it "converts 1 to 'I' "
+          (it "converts 1 to I"
             (should= "I" (to-roman 1)))
-          (it "converts 2 to 'II "
+          (it "converts 2 to II"
             (should= "II" (to-roman 2)))
+          (it "converts 3 to III"
+            (should= "III" (to-roman 3)))
+          (it "converts 4 to IV)"
+            (should= "IV" (to-roman 4)))
 )
--- clojure-katas/roman-numerals/src/roman_numerals/core.clj
+++ clojure-katas/roman-numerals/src/roman_numerals/core.clj
@@ -1,7 +1,10 @@
 (ns roman-numerals.core)
 
 (defn to-roman [number]
-  (loop [number number result ""]
+  (loop [number number 
+         result ""]
     (if (< number 1) 
       result
-      (recur (- number 1) (str result "I")))))
+      (if (= number 4) 
+        "IV"
+      (recur (- number 1) (str result "I"))))))
