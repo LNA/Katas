b5f365b6f9673379a863eb9f09b0ce5456e1ac10
LNA <LAllen@8thLight.com>
2014-07-24 16:50:27 -0500

    Adds fifth test with refactor

--- clojure-katas/roman-numerals/spec/roman_numerals/core_spec.clj
+++ clojure-katas/roman-numerals/spec/roman_numerals/core_spec.clj
@@ -2,12 +2,11 @@
   (:require [speclj.core :refer :all]
             [roman-numerals.core :refer :all]))
 (describe "roman-numerals"
-          (it "converts 1 to I"
-            (should= "I" (to-roman 1)))
-          (it "converts 2 to II"
-            (should= "II" (to-roman 2)))
-          (it "converts 3 to III"
-            (should= "III" (to-roman 3)))
-          (it "converts 4 to IV)"
-            (should= "IV" (to-roman 4)))
+          (it "converts natural numbers to roman numerals"
+            (should= "I"   (to-roman 1))
+            (should= "II"  (to-roman 2))
+            (should= "III" (to-roman 3))
+            (should= "IV"  (to-roman 4))
+            (should= "V"   (to-roman 5))
+          )
 )
--- clojure-katas/roman-numerals/src/roman_numerals/core.clj
+++ clojure-katas/roman-numerals/src/roman_numerals/core.clj
@@ -1,10 +1,11 @@
 (ns roman-numerals.core)
 
+(def roman-numerals [[5 "V"] [4 "IV"] [1 "I"]])
+
 (defn to-roman [number]
-  (loop [number number 
-         result ""]
-    (if (< number 1) 
-      result
-      (if (= number 4) 
-        "IV"
-      (recur (- number 1) (str result "I"))))))
+  (loop [number number result "" numerals roman-numerals]
+    (if (< number 1) result
+      (let [[natural roman] (first numerals)]
+        (if (> natural number )
+          (recur number result (rest numerals))
+          (recur (- number natural) (str result roman) numerals))))))
