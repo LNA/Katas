5a2b4b355f9a1c87681a3f9bdc7fe849595de1ba
LNA <LAllen@8thLight.com>
2014-10-07 12:31:11 -0500

    Adds code before big refactor

--- java-katas/coin-changer/src/com/lallen/kata/CoinChanger.java
+++ java-katas/coin-changer/src/com/lallen/kata/CoinChanger.java
@@ -1,8 +1,35 @@
 package com.lallen.kata;
 
-public class CoinChanger {
-    public static int giveChange(int i) {
-    return i;
-    }
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
+public class CoinChanger {
+
+    public ArrayList<Integer> giveChange(int amountToChange) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        int count = amountToChange;
+        for(int i=0 ; i < count; i++ ) {
+
+            while (count >= 25) {
+                result.add(25);
+                count = count - 25;
+            }
+
+            while  (count >= 10) {
+                result.add(10);
+                count = count - 10;
+            }
+            if  (count >= 5) {
+                result.add(5);
+                count = count - 5;
+            }
+
+            if (count >= 1) {
+                result.add(1);
+            }
+
+        }
+        return result;
+    }
 }
--- java-katas/coin-changer/src/com/lallen/kata/CoinChangerTest.java
+++ java-katas/coin-changer/src/com/lallen/kata/CoinChangerTest.java
@@ -2,14 +2,156 @@ package com.lallen.kata;
 
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import org.junit.Before;
+
 import static junit.framework.Assert.assertEquals;
 
 public class CoinChangerTest {
+    private CoinChanger coinChanger;
+
+    private ArrayList<Integer> expectChange(int... numbers) {
+        ArrayList<Integer> change = new ArrayList<Integer>();
+        for (int number : numbers){
+            change.add(number);
+        }
+        return change;
+    }
+
+    @Before
+    public void setUp() throws  Exception {
+       coinChanger = new CoinChanger();
+    }
 
     @Test
     public void givesChangeFor1() throws Exception {
-        CoinChanger coinChanger = new CoinChanger();
-        assertEquals(1, coinChanger.giveChange(1));
+        assertEquals(expectChange(1), coinChanger.giveChange(1));
+    }
+
+    @Test
+    public  void  givesChangeFor2() throws Exception {
+        assertEquals(expectChange(1, 1), coinChanger.giveChange(2));
+    }
+
+    @Test
+    public void givesChangeFor3()throws Exception {
+        assertEquals(expectChange(1,1,1), coinChanger.giveChange(3));
+    }
+
+    @Test
+    public void givesChangeFor4() throws Exception {
+        assertEquals(expectChange(1,1,1,1), coinChanger.giveChange(4));
+    }
+
+    @Test
+    public void givesChangeFor5() throws Exception {
+        assertEquals(expectChange(5), coinChanger.giveChange(5));
+    }
+
+    @Test
+    public  void givesChangeFor6() throws  Exception {
+        assertEquals(expectChange(5,1), coinChanger.giveChange(6));
+    }
+
+    @Test
+    public void givesChangeFor7() throws Exception {
+        assertEquals(expectChange(5,1,1), coinChanger.giveChange(7));
+    }
+
+    @Test
+    public void givesChangeFor8() throws Exception {
+        assertEquals(expectChange(5,1,1,1), coinChanger.giveChange(8));
+    }
+
+    @Test
+    public void givesChangeFor9() throws Exception {
+        assertEquals(expectChange(5,1,1,1,1), coinChanger.giveChange(9));
+    }
+
+    @Test
+    public void givesChangeFor10() throws Exception {
+        assertEquals(expectChange(10), coinChanger.giveChange(10));
+    }
+
+    @Test
+    public void givesChangeFor11() throws Exception {
+        assertEquals(expectChange(10,1), coinChanger.giveChange(11));
+    }
+
+    @Test
+    public void givesChangeFor12() throws Exception {
+        assertEquals(expectChange(10,1,1), coinChanger.giveChange(12));
+    }
+
+    @Test
+    public void givesChangeFor13() throws Exception {
+        assertEquals(expectChange(10,1,1,1), coinChanger.giveChange(13));
+    }
+
+    @Test
+    public void givesChangeFor14() throws Exception {
+        assertEquals(expectChange(10, 1,1,1,1), coinChanger.giveChange(14));
+    }
+
+    @Test
+    public void givesChangeFor15() throws Exception {
+        assertEquals(expectChange(10, 5), coinChanger.giveChange(15));
+    }
+
+    @Test
+    public  void givesChangeFor16() throws Exception {
+        assertEquals(expectChange(10,5,1), coinChanger.giveChange(16));
+    }
+
+    @Test
+    public void givesChangeFor17() throws Exception {
+        assertEquals(expectChange(10, 5,1,1), coinChanger.giveChange(17));
+    }
+
+    @Test
+    public void givesChangeFor18() throws Exception {
+        assertEquals(expectChange(10, 5,1,1,1), coinChanger.giveChange(18));
+    }
+
+    @Test
+    public void givesChangeFor19() throws Exception {
+        assertEquals(expectChange(10, 5, 1, 1, 1, 1), coinChanger.giveChange(19));
+    }
+
+    @Test
+    public void givesChangeFor20() throws Exception {
+        assertEquals(expectChange(10,10), coinChanger.giveChange(20));
+    }
+
+    @Test
+    public void givesChangeFor21() throws Exception {
+        assertEquals(expectChange(10,10,1), coinChanger.giveChange(21));
+    }
+
+    @Test
+    public  void  givesChangeFor22() throws Exception {
+        assertEquals(expectChange(10,10,1,1), coinChanger.giveChange(22));
+    }
+
+    @Test
+    public void givesChangeFor23() throws Exception {
+        assertEquals(expectChange(10, 10,1,1,1), coinChanger.giveChange(23));
+    }
+
+    @Test
+    public void givesChangeFor24() throws Exception {
+        assertEquals(expectChange(10,10,1,1,1,1), coinChanger.giveChange(24));
+    }
+
+    @Test
+    public void givesChangeFor25() throws Exception {
+        assertEquals(expectChange(25), coinChanger.giveChange(25));
+    }
+
+    @Test
+    public void givesChangeFor76() throws Exception {
+        assertEquals(expectChange(25,25,25,1), coinChanger.giveChange(76));
     }
 
 }
