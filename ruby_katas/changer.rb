ef0876728b579561b5b612869d0bb6bec8b1a8d8
LNA <LAllen@8thLight.com>
2014-11-09 19:10:33 -0600

    fin

--- ruby_katas/changer.rb
+++ ruby_katas/changer.rb
@@ -2,26 +2,12 @@ class CoinChanger
   def make_change(n)
     change = []
     coin_denominations = [25, 10, 5, 1]
-    case n
-    when 1..50
-      while n >= 25
-        n -= 25
-        change << 25
+    coin_denominations.each do |coin|
+      while n >= coin
+        change << coin
+        n -= coin
       end
-      while n >= 10
-        change << 10
-        n -= 10
-      end
-      while n >= 5
-        change << 5
-        n -= 5
-      end
-      if n >= 1
-        n.times do
-          change << 1
-        end
-      end
-      change
     end
+    change
   end
 end
