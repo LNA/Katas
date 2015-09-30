bcd21d9b3ee349c39e2c49d96cfa122702a267e7
LNA <LaToyaNAllen@gmail.com>
2015-09-29 20:23:10 -0600

    [RUST]Red for prime factor of 4

--- rust_katas/prime_factors/tests/lib.rs
+++ rust_katas/prime_factors/tests/lib.rs
@@ -2,6 +2,16 @@ extern crate prime_factors;
 use prime_factors::prime;
 
 #[test]
-fn of_two() {
-    assert_eq!(prime(2), [2]);
+fn it_gives_two_for_two() {
+  assert_eq!(prime(2), [2]);
+}
+
+#[test]
+fn it_gives_three_for_three() {
+  assert_eq!(prime(3), [3]);
+}
+
+#[test]
+fn it_gives_two_and_two_for_four() {
+  assert_eq!(prime(4), [2,2])
 }
